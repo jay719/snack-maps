fd38c9af4ca2fbbb69158391e12af402ce99fd47
jay719 <javariab17@gmail.com>
2021-01-04 11:20:32 -0700

    Added auth up until step 2

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,21 @@
+class AuthenticationController < ApplicationController
+    def login
+        @user = User.find_by username: params[:username]
+
+        if !@user 
+            render json: { error: "No user with that username" }, status: :unauthorized
+        else 
+            if !@user.authenticate params[:password]
+                render json: { message: "Nah bruh"}, status: :unauthorized
+            else
+             payload = {
+                    catchphrase: "Not the mama!",
+                    user_id: @user.id
+                }
+                secret = "topsecret"
+                token = JWT.encode payload, secret
+                render json:{token: token}, status: :created
+            end
+        end
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,12 @@ class UsersController < ApplicationController
             password: params[:password]
         )
         render json:{user: @user}, status: :created
+    
+    end
+    
+    def index
+        @users = User.all
+
+        render json: @users
     end
 end
