349ad6a821a6c68a579295be1fa4fb539ae5f2e8
jay719 <javariab17@gmail.com>
2021-01-06 11:18:29 -0700

    Access to Zomato api and latitude/long of resturaunts

--- front-end/app.js
+++ front-end/app.js
@@ -19,24 +19,12 @@ const circle = L.circle([39.90, -106.09], {
     radius: 500
 }).addTo(mymap);
 
-var polygon = L.polygon([
-    [39.7348, -104.9653],
-    [39.7312, -104.950],
-    [39.74, -104.97]
-]).addTo(mymap);
+const welcomeMessage = '<h1>Hello User welcome to the app</h1>'
 
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker.bindPopup(welcomeMessage).openPopup();
 circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
 
-var popup = L.popup()
-    .setLatLng([39.7348, -104.9653])
-    .setContent("Welcome to the Nutrition finder app.")
-    .openOn(mymap);
-
-    var popup = L.popup();
-
+var popup = L.popup();
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
@@ -45,3 +33,36 @@ function onMapClick(e) {
 }
 
 mymap.on('click', onMapClick);
+
+const spawnURL = "https://developers.zomato.com/api/v2.1/geocode?lat=39.73892&lon=-104.9850";
+const spawnButton = document.querySelector('#spawn')
+
+spawnButton.addEventListener('click', findNearbyRestuaraunts)
+
+function findNearbyRestuaraunts(event){ 
+    event.preventDefault()
+    fetch(spawnURL, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "user-key": "a6631f3561d12ac2fa67b3d8cc55d409",
+        },
+    
+    })
+    .then(parseJSON)
+    .then(apiObject=> apiObject.nearby_restaurants.forEach(restaurantIndex => {
+        const location = restaurantIndex.restaurant.location;
+        console.log(apiObject)
+        const longitude = location.longitude;
+        const latitude = location.latitude;
+        console.log(latitude, longitude);
+        const restaurantMarker = L.marker([latitude, longitude]).addTo(mymap);
+
+})
+
+)
+
+function parseJSON(response){
+return response.json()
+}
+   }
