467651a4962d83c7f6007794cbce76706ba8df59
jay719 <javariab17@gmail.com>
2021-01-06 12:15:24 -0700

    changed resturant icon and added name to popup

--- front-end/app.js
+++ front-end/app.js
@@ -12,17 +12,28 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 const marker = L.marker([39.70, -104.989]).addTo(mymap);
 
-const circle = L.circle([39.90, -106.09], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
+// const circle = L.circle([39.71, -104.989], {
+//     color: 'red',
+//     fillColor: '#8470ff3',
+//     fillOpacity: 0.5,
+//     radius: 1000
+// }).addTo(mymap);
+
+var restaurantIcon= L.icon({
+    iconUrl: 'restIcon.png',
+    shadowUrl: 'shadow.png',
+
+    iconSize:     [38, 50], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [22, 94],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 const welcomeMessage = '<h1>Hello User welcome to the app</h1>'
 
 marker.bindPopup(welcomeMessage).openPopup();
-circle.bindPopup("I am a circle.");
+// circle.bindPopup("I am a circle.");
 
 var popup = L.popup();
 function onMapClick(e) {
@@ -51,13 +62,19 @@ function findNearbyRestuaraunts(event){
     })
     .then(parseJSON)
     .then(apiObject=> apiObject.nearby_restaurants.forEach(restaurantIndex => {
-        const location = restaurantIndex.restaurant.location;
-        console.log(apiObject)
+        const restaurant = restaurantIndex.restaurant
+        const location = restaurant.location;
+        const img = `<img src="${restaurant.featured_image}">`
+        const name = restaurant.name
+        // console.log(apiObject)
+        console.log(restaurant);
+        console.log(img);
+
         const longitude = location.longitude;
         const latitude = location.latitude;
-        console.log(latitude, longitude);
-        const restaurantMarker = L.marker([latitude, longitude]).addTo(mymap);
 
+        const restaurantMarker = L.marker([latitude, longitude], {icon:restaurantIcon}).addTo(mymap);
+        restaurantMarker.bindPopup(name,img).openPopup();
 })
 
 )
