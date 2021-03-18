8a42d6bd019bdec6cac3027227ddbe362c59b3f8
jay719 <javariab17@gmail.com>
2021-03-17 21:15:02 -0600

    added firebbase through config

---
+++ front-end/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD6t7CaxOIWBxvH1QHYea5WTI3qhdYERoI",
+  authDomain: "mod-3-project-7a3d5.firebaseapp.com",
+  projectId: "mod-3-project-7a3d5",
+  storageBucket: "mod-3-project-7a3d5.appspot.com",
+  messagingSenderId: "404135491345",
+  appId: "1:404135491345:web:c7c1a653a9820b0c2f3eb0",
+  measurementId: "G-7G11ZNT76B"
+};
