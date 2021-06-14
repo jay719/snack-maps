054aff240215b2b66e821ee0239354c69212ac93
jay719 <javariab17@gmail.com>
2021-06-13 22:56:02 -0600

    added test for logging in

--- test-cafe/LoginSequenceTest.js
+++ test-cafe/LoginSequenceTest.js
@@ -4,7 +4,7 @@ import { Selector } from "testcafe";
 fixture `Log in to Snack Maps`
     .page `https://snack-maps-7.web.app/`
 
-test('Click Sign in Buttom input credentials and verify we go back to log in page', async t => {
+test('Click Sign in Buttom input credentials and verify signed in message appears', async t => {
     const submitSignInButton = Selector('.submit-signin')
     const signedInMessage = Selector('.message2')
     const signInButton = Selector('#signin-btn')
