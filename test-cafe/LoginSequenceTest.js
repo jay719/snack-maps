b9fa23c333729084234a02797c609d68e46a629b
jay719 <javariab17@gmail.com>
2021-06-13 22:55:28 -0600

    added test for logging in

--- test-cafe/LoginSequenceTest.js
+++ test-cafe/LoginSequenceTest.js
@@ -2,9 +2,22 @@ import { sign } from "crypto";
 import { Selector } from "testcafe";
 
 fixture `Log in to Snack Maps`
-    .page 'https://snack-maps-7.web.app/';
+    .page `https://snack-maps-7.web.app/`
 
 test('Click Sign in Buttom input credentials and verify we go back to log in page', async t => {
+    const submitSignInButton = Selector('.submit-signin')
+    const signedInMessage = Selector('.message2')
     const signInButton = Selector('#signin-btn')
-        await t.click(signInButton)
+    const signInPassword = Selector('#password')
+    const signInUserName = Selector('#username')
+
+        await t
+            .click(signInButton)
+            .expect(submitSignInButton.visible).eql(true)
+            .typeText(signInUserName, 'tes')
+            .typeText(signInPassword, 'tes')
+            .click(submitSignInButton)
+            .expect(signedInMessage.visible).eql(true)
+            console.log('done')
+
 })
