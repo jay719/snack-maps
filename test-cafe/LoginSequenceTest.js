9aced99b5f5c63387c655ecb0d6d77caefac3d39
jay719 <javariab17@gmail.com>
2021-06-13 20:58:22 -0600

    should be good

--- test-cafe/LoginSequenceTest.js
+++ test-cafe/LoginSequenceTest.js
@@ -1,8 +1,10 @@
+import { sign } from "crypto";
 import { Selector } from "testcafe";
 
 fixture `Log in to Snack Maps`
     .page 'https://snack-maps-7.web.app/';
 
 test('Click Sign in Buttom input credentials and verify we go back to log in page', async t => {
-    const signInButton = Selector()
+    const signInButton = Selector('#signin-btn')
+        await t.click(signInButton)
 })
